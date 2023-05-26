2dc891f42674ece6cdfbecde324982d0cc7a9b56
Mercado <ricardo.mercado@accenture.com>
2023-05-26 11:38:12 -0600

    migrate project to maven

---
+++ src/main/java/Model/Person.java
@@ -0,0 +1,20 @@
+package Model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor @ToString
+public class Person {
+
+    @Getter @Setter
+    private String name;
+
+    @Getter @Setter
+    private int age;
+
+    @Getter @Setter
+    private String country;
+
+}
